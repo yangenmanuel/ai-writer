2c20fc83c379bc18a8e087bdb1193510135fa381
Yang Enmanuel <enmanuelcalzat@gmail.com>
2023-01-24 22:06:54 -0300

    Preparing project structure

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ai Message Generator</title>
+        <meta name="description" content="An ai-provided tool that generates messages, emails, and letters for you" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Ai Message Generator</h1>
+      </main>
+    </>
+  )
+}

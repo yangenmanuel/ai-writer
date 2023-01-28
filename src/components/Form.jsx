f2668b0f19b55ef93e23b6b5245af1569dd8f9eb
Yang Enmanuel <enmanuelcalzat@gmail.com>
2023-01-28 17:24:22 -0300

    Customized Form background

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -27,10 +27,10 @@ export default function Form ({ setMessage, setLoading }) {
     }
 
   return (
-    <form onSubmit={handleSubmit} className='border border-orange-500 p-3 rounded-lg '>
+    <form onSubmit={handleSubmit} className='p-3 rounded-lg bg-[#313135]'>
       <div className='mb-4'>
         <label htmlFor='type' className="block mb-2 text-sm font-medium">Type of message</label>
-        <select name='type' className="bg-black-300 text-sm rounded-lg border-2 border-black border-opacity-30 focus:border-orange-500 focus:border-2 focus:outline-none block w-full p-2.5 mb-3" onChange={handleChange}>
+        <select name='type' className="bg-[#3c3c3e] text-sm rounded-lg border-2 border-[#474749] border-opacity-30 focus:border-orange-500 focus:border-2 focus:outline-none block w-full p-2.5 mb-3" onChange={handleChange}>
           <option value='text message'>Text Message</option>
           <option value='email'>Email</option>
           <option value='letter'>Letter</option>
@@ -39,12 +39,12 @@ export default function Form ({ setMessage, setLoading }) {
 
       <div className='mb-4'>
         <label htmlFor='toWho' className="block mb-2 text-sm font-medium">Receiver:</label>
-        <input type='text' name='toWho' className="text-sm rounded-lg border-2 border-black border-opacity-30 focus:border-orange-500 focus:border-2 outline-none block w-full p-2.5 bg-black-300 placeholder-gray-400 shadow" placeholder="Ex: My friend / My boss" onChange={handleChange}/>
+        <input type='text' name='toWho' className="bg-[#3c3c3e] text-sm rounded-lg border-2 border-[#474749] border-opacity-30 focus:border-orange-500 focus:border-2 outline-none block w-full p-2.5 bg-black-300 placeholder-gray-400 shadow" placeholder="Ex: My friend / My boss" onChange={handleChange}/>
       </div>
 
       <div className='mb-4'>
         <label htmlFor='tone' className="block mb-2 text-sm font-medium">Tone:</label>
-        <select name='tone' className="bg-black-300 text-sm rounded-lg border-2 border-black border-opacity-30 focus:border-orange-500 focus:border-2 focus:outline-none block w-full p-2.5 mb-3" onChange={handleChange}>
+        <select name='tone' className="bg-[#3c3c3e] text-sm rounded-lg border-2 border-[#474749] border-opacity-30 focus:border-orange-500 focus:border-2 focus:outline-none block w-full p-2.5 mb-3" onChange={handleChange}>
           <option value='neutral'>Neutral</option>
           <option value='friendly'>Friendly</option>
           <option value='professional'>Professional</option>
@@ -55,7 +55,7 @@ export default function Form ({ setMessage, setLoading }) {
       <div className='mb-4'>
         <label htmlFor='about' className="block mb-2 text-sm font-medium">What is it about?</label>
         <textarea onChange={handleChange}
-          className='resize-none w-full h-40 rounded-lg p-3 border-2 border-black border-opacity-30 focus:border-orange-500 focus:outline-none'
+          className='bg-[#3c3c3e] resize-none w-full h-40 rounded-lg p-3 border-2 border-[#474749] border-opacity-30 focus:border-orange-500 focus:outline-none'
           name='about'
           placeholder='Telling my friend I love her...'
         ></textarea>
